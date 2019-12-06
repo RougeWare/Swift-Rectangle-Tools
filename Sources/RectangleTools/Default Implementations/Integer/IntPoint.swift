38310fc4c40f4780eb728c524a120927d8ca4931
Ben Leggiero <BenLeggiero@Gmail.com>
2019-12-05 19:42:06 -0700

    #11: Introduced `Size2DCollection`

--- Sources/RectangleTools/Default Implementations/Integer/IntPoint.swift
+++ Sources/RectangleTools/Default Implementations/Integer/IntPoint.swift
@@ -25,6 +25,7 @@ public struct BinaryIntegerPoint<Length: BinaryInteger>: MutablePoint2D {
 
 
 
+// MARK: - Common types
 
 public typealias IntPoint = BinaryIntegerPoint<Int>
 public typealias Int8Point = BinaryIntegerPoint<Int8>
@@ -37,3 +38,11 @@ public typealias UInt8Point = BinaryIntegerPoint<UInt8>
 public typealias UInt16Point = BinaryIntegerPoint<UInt16>
 public typealias UInt32Point = BinaryIntegerPoint<UInt32>
 public typealias UInt64Point = BinaryIntegerPoint<UInt64>
+
+
+
+// MARK: - Default conformances
+
+extension BinaryIntegerPoint: Equatable {}
+extension BinaryIntegerPoint: Hashable {}
+extension BinaryIntegerPoint: Codable where Length: Codable {}
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -12,4 +12,5 @@ import RectangleToolsTests
 
 var tests = [XCTestCaseEntry]()
 tests += RectangleToolsTests.allTests()
+tests += IntegerSizeTests.allTests()
 XCTMain(tests)
