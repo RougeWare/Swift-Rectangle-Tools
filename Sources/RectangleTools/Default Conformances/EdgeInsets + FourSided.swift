213c845fdc7210ece18811e5d1d3b5c9b562ad58
Ben Leggiero <BenLeggiero@Gmail.com>
2021-07-21 15:08:17 -0600

    Treating SwiftUI as a native framework
    It's clear that Apple is moving forward with SwiftUI as the future of all their platforms, so I'll treat it like that too

--- Sources/RectangleTools/Default Conformances/EdgeInsets + FourSided.swift
+++ Sources/RectangleTools/Default Conformances/EdgeInsets + FourSided.swift
@@ -6,7 +6,13 @@
 //  Copyright © 2020 Ben Leggiero. All rights reserved.
 //
 
-#if canImport(WatchKit)
+#if canImport(SwiftUI)
+    import SwiftUI
+
+    public typealias NativeEdgeInsets = SwiftUI.EdgeInsets
+    @available(watchOS 2.1, *)
+    public typealias UserInterfaceLayoutDirection = SwiftUI.LayoutDirection
+#elseif canImport(WatchKit)
     import WatchKit
 
     public typealias NativeEdgeInsets = UIEdgeInsets
@@ -28,6 +34,56 @@
 
 @available(watchOS 2.1, *)
 extension NativeEdgeInsets: FourSidedAbsolute {
+    
+    #if canImport(SwiftUI)
+    
+    public init(top: CGFloat, right: CGFloat, bottom: CGFloat, left: CGFloat) {
+        switch UserInterfaceLayoutDirection.current {
+        case .leftToRight:
+            self.init(top: top, leading: left, bottom: bottom, trailing: right)
+            
+        case .rightToLeft:
+            self.init(top: top, leading: right, bottom: bottom, trailing: left)
+            
+        @unknown default:
+            assertionFailure("Unknown user interface layout direction (will assume LTR): \(UserInterfaceLayoutDirection.current)")
+            self.init(top: top, leading: left, bottom: bottom, trailing: right)
+        }
+    }
+    
+    
+    public init(top: CGFloat, trailing: CGFloat, bottom: CGFloat, leading: CGFloat) {
+        self.init(top: top, leading: leading, bottom: bottom, trailing: trailing)
+    }
+    
+    
+    public var left: CGFloat {
+        switch UserInterfaceLayoutDirection.current {
+        case .leftToRight: return leading
+        case .rightToLeft: return trailing
+            
+        @unknown default:
+            assertionFailure("Unknown user interface layout direction (will assume LTR): \(UserInterfaceLayoutDirection.current)")
+            return leading
+        }
+    }
+    
+    
+    public var right: CGFloat {
+        switch UserInterfaceLayoutDirection.current {
+        case .leftToRight: return trailing
+        case .rightToLeft: return leading
+            
+        @unknown default:
+            assertionFailure("Unknown user interface layout direction (will assume LTR): \(UserInterfaceLayoutDirection.current)")
+            return trailing
+        }
+    }
+    
+    
+    #else
+    
+    
     @available(watchOS 2.1, *)
     public init(top: CGFloat, right: CGFloat, bottom: CGFloat, left: CGFloat) {
         self.init(top: top, left: left, bottom: bottom, right: right)
@@ -47,10 +103,13 @@ extension NativeEdgeInsets: FourSidedAbsolute {
             self.init(top: top, right: trailing, bottom: bottom, left: leading)
         }
     }
+    
+    #endif
 }
 
 
 
+#if !canImport(SwiftUI)
 @available(watchOS 2.1, *)
 public extension NativeEdgeInsets {
     /// The value of whichever edge inset is leading in the current app's UI direction
@@ -76,11 +135,19 @@ public extension NativeEdgeInsets {
         }
     }
 }
+#endif
 
 
 
 #if !canImport(UIKit) && canImport(AppKit)
-extension NSEdgeInsets: Equatable {}
+extension NSEdgeInsets: Equatable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.top == rhs.top
+            && lhs.right == rhs.right
+            && lhs.bottom == rhs.bottom
+            && lhs.left == rhs.left
+    }
+}
 #endif
 
 
@@ -90,12 +157,25 @@ public extension UserInterfaceLayoutDirection {
     // TODO: Move this to some other package
     @inline(__always)
     static var current: UserInterfaceLayoutDirection {
-        #if canImport(WatchKit)
-        return WKInterfaceDevice.current().layoutDirection
+        #if canImport(SwiftUI)
+            #if canImport(UIKit)
+                let legacyCurrent = UIApplication.shared.userInterfaceLayoutDirection
+            #elseif canImport(AppKit)
+                let legacyCurrent = NSApp?.userInterfaceLayoutDirection ?? .leftToRight
+            #endif
+            switch legacyCurrent {
+            case .leftToRight: return .leftToRight
+            case .rightToLeft: return .rightToLeft
+            @unknown default:
+                assertionFailure("Unknown user interface layout direction (will assume LTR): \(UserInterfaceLayoutDirection.current)")
+                return .leftToRight
+            }
+        #elseif canImport(WatchKit)
+            return WKInterfaceDevice.current().layoutDirection
         #elseif canImport(UIKit)
-        return UIApplication.shared.userInterfaceLayoutDirection
+            return UIApplication.shared.userInterfaceLayoutDirection
         #elseif canImport(AppKit)
-        return NSApp?.userInterfaceLayoutDirection ?? .leftToRight
+            return NSApp?.userInterfaceLayoutDirection ?? .leftToRight
         #endif
     }
 }
